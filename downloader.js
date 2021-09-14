42b6d111ed9f43a5c08aca572c1fb4633d5a3f75
Benedict Chen <benedict@benedictchen.com>
2021-09-14 02:41:06 -0700

    Update downloader.js

--- downloader.js
+++ downloader.js
@@ -28,14 +28,14 @@ x = async function(buttons, i) {
     let button = buttons[i];
     if (!button) return;
     console.log(new Date(), 'clicking button', {button});
-    button.click();
+    button?.click();
     console.log(new Date(), 'waiting 3 seconds');
     await new Promise((resolve) => setTimeout(resolve, 3000));
     console.log(new Date(), 'clicking download');
-    Array.from(document.querySelectorAll('a')).filter(el => el.innerText === DOWNLOAD_PDF_BTN_TEXT).pop().click();
+    Array.from(document.querySelectorAll('a')).filter(el => el.innerText === DOWNLOAD_PDF_BTN_TEXT).pop()?.click();
     console.log(new Date(), 'waiting 5 seconds');
     await new Promise((resolve) => setTimeout(resolve, 5000));
-    document.querySelector('button[aria-label=' + CLOSE_BUTTON_TEXT + ']').click()
+    document.querySelector('button[aria-label=' + CLOSE_BUTTON_TEXT + ']')?.click()
     console.log(new Date(), 'moving onto next...');
     if (buttons[i + 1]) {
       x(buttons, i + 1);
